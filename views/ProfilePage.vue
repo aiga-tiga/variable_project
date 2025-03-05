eead15dcd5bffaecbb2f80fbeae69339720f6862
aiga-tiga <92212191+aiga-tiga@users.noreply.github.com>
2025-03-05 17:17:50 +0500

    Add files via upload

---
+++ views/ProfilePage.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <div
+      class="profile_background"
+      :style="{ backgroundImage: `url(${backgroundURL})` }"
+    >
+      <div>
+        <h1>Profile</h1>
+        <p>ID: {{ userId }}</p>
+      </div>
+    </div>
+    <div class="container profile_mainInfo">
+      <div class="pfp"></div>
+    </div>
+  </header>
+</template>
+
+<script>
+import { useRoute } from "vue-router";
+export default {
+  setup() {
+    const route = useRoute();
+    return {
+      userId: route.params.id,
+    };
+  },
+  props: {
+    backgroundURL: {
+      type: String,
+      default: "/images/pexels-pixabay-33597.jpg",
+    },
+  },
+};
+</script>
+
+<style scoped>
+.profile_background {
+  height: 300px;
+  background-size: cover;
+  background-position: center;
+}
+</style>
